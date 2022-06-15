329bfb7ced7633669aea8d1df5004e4d6d7b5419
Prasanna <polanaprasanna@gmail.com>
2022-06-15 14:03:21 +0530

    enters added

--- jardemo/src/main/java/com/jar/jardemo/JardemoApplication.java
+++ jardemo/src/main/java/com/jar/jardemo/JardemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JardemoApplication {
 
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(JardemoApplication.class, args);
 	}
